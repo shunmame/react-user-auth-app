83fccd7a665e0b17a7d6d797361508dc13d15b4c
shunmame <32508729+shunmame@users.noreply.github.com>
2022-11-01 22:01:53 +0900

    API作成

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,19 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
 });
 
+app.get("/api/auth", async (req, res) => {
+  const idToken = req.header("Authorization");
+  if (idToken) {
+    const {uid} = await admin.auth().verifyIdToken(idToken);
+    // uid を使って紐付けられたユーザー情報を取得する処理を行ったりする。
+    // const someUseInfo = userService.getInfo(uid);
+    console.log(uid)
+    res.json(idToken);
+  }
+  // Authorizationヘッダーが無ければ403
+  res.status(403).send();
+});
+
 app.listen(port, () => {
   console.log(`listening on *:${port}`);
 })
--- frontend/src/User.js
+++ frontend/src/User.js
@@ -1,18 +1,49 @@
-// import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { auth, _ } from './firebase';
 import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import axios from "axios";
 
 function User() {
-  // const [message, setMessage] = useState('');
-  // useEffect(() =>{
-  //   fetch('/api')
-  //     .then((res) => res.json())
-  //     .then((data) => setMessage(data.message));
-  // },[])
+  const [message, setMessage] = useState('');
+  useEffect(() =>{
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[])
+
+  const navigate = useNavigate();
+  const [user, setUser] = useState("");
+  useEffect(() => {
+
+    onAuthStateChanged(auth, async (currentUser) => {
+      setUser(currentUser);
+
+      if (currentUser) {
+        const idToken = await currentUser.getIdToken();
+        // 何らかの認証が必要なリクエストをIDトークン付きで飛ばす
+        const res = await axios.get(
+          "/api/auth",
+          {
+            headers: {
+              Authorization: idToken
+            }
+          }
+        );
+        // console.log(res.data);
+      } else {
+        console.log("please login");
+        navigate("/login")
+      }
+    }, []);
+  })
+
   return (
     <div className="User">
       <h1>ユーザー個人の画面</h1>
-      {/* <p>{ message }</p> */}
+      <p>{user.email}</p>
+      <p>{user.displayName}</p>
+      <p>{ message }</p>
       <SignOutButton />
     </div>
   );
@@ -20,16 +51,15 @@ function User() {
 
 export default User;
 
-
 function SignOutButton() {
   const navigate = useNavigate();
-  
+
   const signOutWithGoogle = () => {
-    auth.signOut().then((result)  => {
-        console.log(result);
-        navigate("/login");
+    auth.signOut().then((result) => {
+      console.log(result);
+      navigate("/login");
     })
-};
+  };
   return (
     <button onClick={signOutWithGoogle}>
       <p>サインアウト</p>
