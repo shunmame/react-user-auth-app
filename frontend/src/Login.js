ae8a4ebf165ca0e08534be4d332b5b7133095765
shunmame <ap1570mats@g.kumamoto-nct.ac.jp>
2022-11-02 09:44:26 +0900

    ログイン時リダイレクト

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,33 +1,39 @@
 import { auth, provider } from './firebase';
 import { signInWithPopup, signInWithRedirect, onAuthStateChanged } from 'firebase/auth';
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return (
-    <div>
-        <SiginButton></SiginButton>
-    </div>
-  )
+    const navigate = useNavigate();
+    useEffect(() => {
+        onAuthStateChanged(auth, async (currentUser) => {
+            if (currentUser) {
+                navigate("/user")
+            }
+        }, []);
+    })
+    
+    return (
+        <div>
+            <SiginButton></SiginButton>
+        </div>
+    )
 }
 
 export default Login
 
-function SiginButton(){
+function SiginButton() {
     const navigate = useNavigate();
 
     const signInWithGoogle = () => {
-        signInWithPopup(auth, provider).then((result)  => {
-            console.log(result);
-            console.log(auth)
+        signInWithPopup(auth, provider).then((result) => {
             navigate("/user");
         })
         // signInWithRedirect(auth, provider).then((result)  => {
         //     console.log(result);
         //     navigate("/user");
         // })
-    };
+    }
 
     return (
         <button onClick={signInWithGoogle}>
